a3cb47f3a87ab05e85f32c2345d5c1b8f121f9fe
Ksenia2772 <ksu.mandrik@bk.ru>
2026-04-28 12:59:39 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$db_host = 'localhost';
+$db_user = 'u82194';
+$db_pass = '8381502';
+$db_name = 'u82194';
+
+try {
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Ошибка подключения к базе данных: " . $e->getMessage());
+}
+?>
